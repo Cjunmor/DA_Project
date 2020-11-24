9404107d4d8777c719120d14ecc2a37a19f71678
Carolina <carolinajuncam@yahoo.com>
2020-11-24 12:37:00 +0100

    commited v

--- 00_import_xlsx.R
+++ 00_import_xlsx.R
@@ -1,23 +1,11 @@
 ###IMPORT DATA TO DATA FOLDER IN YOUR COMPUTER
 install.packages("readxl")
-install.packages("gdata")
 library(readxl)
-library(gdata)
 
-#sdgf
-
+#Bacteria IAA and P concentrations
 xl_1 <- "https://dfzljdn9uc3pi.cloudfront.net/2020/9005/1/DatasetS1_Raw_Data__Screening__P_solubilization_and_IAA_production.xlsx"
+download.file(xl_1, "Data/Data_1.xls", mode = "wb")
+
+#Green House morphological trait measures
 xl_2 <- "https://dfzljdn9uc3pi.cloudfront.net/2020/9005/1/Raw_Data_Greenhouse_tests.xlsx"
 download.file(xl_2, "Data/Data_GH.xls", mode = "wb")
-download.file()
-data_s1<- readxl::read_excel(Webcsv, sheet = 1)
- 
-
-(Webcsv, header = T, sep = "\t")
-Data <- read.table(Webcsv, header = T, sep = "\t")
-print(Data)                    # Check if it has been correctly imported
-
-u <- "http://www.abc.com/abc.zip"
-basename(u) # "abc.zip"
-
-download.file(u, file.path("foo", basename(u)))
