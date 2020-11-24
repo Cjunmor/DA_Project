30bd136d3b05310f8a119aa04ee08ad6f2468e41
Carolina <carolinajuncam@yahoo.com>
2020-11-24 12:53:57 +0100

    .xls changed to .xlsx for the files to be readable

--- 00_import_xlsx.R
+++ 00_import_xlsx.R
@@ -1,12 +1,11 @@
 ###IMPORT DATA TO DATA FOLDER IN YOUR COMPUTER
-install.packages("readxl")
+#install.packages("readxl")
 library(readxl)
-#commit ok
 
 #Bacteria IAA and P concentrations
 xl_1 <- "https://dfzljdn9uc3pi.cloudfront.net/2020/9005/1/DatasetS1_Raw_Data__Screening__P_solubilization_and_IAA_production.xlsx"
-download.file(xl_1, "Data/Data_1.xls", mode = "wb")
+download.file(xl_1, "Data/Data_1.xlsx", mode = "wb")
 
 #Green House morphological trait measures
 xl_2 <- "https://dfzljdn9uc3pi.cloudfront.net/2020/9005/1/Raw_Data_Greenhouse_tests.xlsx"
-download.file(xl_2, "Data/Data_GH.xls", mode = "wb")
+download.file(xl_2, "Data/Data_GH.xlsx", mode = "wb")
