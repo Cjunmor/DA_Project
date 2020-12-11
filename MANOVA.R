8afe1ea68cbd1e7ee777fa92a1ce9e260895986e
Nacho Romero <nacho.romerolozano@gmail.com>
2020-12-11 20:09:28 +0100

    Two way MANOVA (Strains and Crop) done with all the dependent variables

--- MANOVA.R
+++ MANOVA.R
@@ -4,7 +4,7 @@
 library(compositions)
 #bind all the dependent variables that will be used in a single object
 
-bind_all <- cbind(D_GH$`Plant high (Mean - cm)`,D_GH$`Shoot dry mass (g)`,D_GH$`Root dry mass (g)`,D_GH$`Total dry mass (g)`, D_GH$`P Soil (mg/dm3)`,D_GH$`P Shoot g/kg`, D_GH$`P Roots g/kg`, DGH)
+bind_all <- cbind(D_GH$`Plant high (Mean - cm)`,D_GH$`Shoot dry mass (g)`,D_GH$`Root dry mass (g)`,D_GH$`Total dry mass (g)`, D_GH$`P Soil (mg/dm3)`,D_GH$`P Shoot g/kg`, D_GH$`P Roots g/kg`, D_GH$`N Shoot g/kg`, D_GH$`N Roots g/kg`)
 
 manova_all <- manova(ilr(clo(bind_all)) ~ D_GH$`Treatments (strains)`*D_GH$Cropname, data = D_GH)
 
