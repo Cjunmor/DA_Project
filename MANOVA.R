cfef3ee7b137d0cbd133ad52255f27523530ab02
Nacho Romero <nacho.romerolozano@gmail.com>
2020-12-11 20:08:25 +0100

    Two Way Manova (Treatments and Crops) for all the dependent variables.

--- MANOVA.R
+++ MANOVA.R
@@ -3,11 +3,16 @@
 #install.packages("compositions")
 library(compositions)
 #bind all the dependent variables that will be used in a single object
-bind <- cbind(D_GH$`Plant high (Mean - cm)`,D_GH$`Shoot dry mass (g)`,D_GH$`Root dry mass (g)`,D_GH$`Total dry mass (g)`)
 
-manova <- manova(ilr(clo(bind)) ~ D_GH$`Treatments (strains)`*D_GH$Cropname, data = D_GH)
+bind_all <- cbind(D_GH$`Plant high (Mean - cm)`,D_GH$`Shoot dry mass (g)`,D_GH$`Root dry mass (g)`,D_GH$`Total dry mass (g)`, D_GH$`P Soil (mg/dm3)`,D_GH$`P Shoot g/kg`, D_GH$`P Roots g/kg`, DGH)
 
-summary(manova)
-summary(manova, test= "Hotelling-Lawley")
-summary(manova, test= "Roy")
-summary(manova, test= "Wilks")
+manova_all <- manova(ilr(clo(bind_all)) ~ D_GH$`Treatments (strains)`*D_GH$Cropname, data = D_GH)
+
+summary(manova_all)
+summary(manova_all, test= "Hotelling-Lawley")
+summary(manova_all, test= "Roy")
+summary(manova_all, test= "Wilks")
+
+bind_bean <-  cbind(D_Bean$`Plant high (Mean - cm)`,D_Bean$`Shoot dry mass (g)`,D_Bean$`Root dry mass (g)`,D_Bean$`Total dry mass (g)`)
+
+
