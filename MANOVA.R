6c5d2a0cb9eb1cd61e4ef11014b99edd74617595
Nacho Romero <nacho.romerolozano@gmail.com>
2020-12-11 20:03:55 +0100

    MANOVA part 2
    Try to make the 3 different manovas for each crop

--- MANOVA.R
+++ MANOVA.R
@@ -2,8 +2,12 @@
 
 #install.packages("compositions")
 library(compositions)
-manova2 <- manova(ilr(clo(bind)) ~ D_GH$`Treatments (strains)`*D_GH$Cropname, data = D_GH)
-summary(manova2)
-summary(manova2, test= "Hotelling-Lawley")
-summary(manova2, test= "Roy")
-summary(manova2, test= "Wilks")
+#bind all the dependent variables that will be used in a single object
+bind <- cbind(D_GH$`Plant high (Mean - cm)`,D_GH$`Shoot dry mass (g)`,D_GH$`Root dry mass (g)`,D_GH$`Total dry mass (g)`)
+
+manova <- manova(ilr(clo(bind)) ~ D_GH$`Treatments (strains)`*D_GH$Cropname, data = D_GH)
+
+summary(manova)
+summary(manova, test= "Hotelling-Lawley")
+summary(manova, test= "Roy")
+summary(manova, test= "Wilks")
