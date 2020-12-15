10b82f9b7bdcf81cc1d67260f88da928101d1cb8
Cjuncam <carolinajuncam@yahoo.com>
2020-12-15 18:03:44 +0100

    Added the crate folder function

--- 00_import_xlsx.R
+++ 00_import_xlsx.R
@@ -2,6 +2,10 @@
 #install.packages("readxl")
 library(readxl)
 
+#Creating the folder "Data" if it doesn't exist
+D <- "Data"
+if (!dir.exists(D)) dir.create(D)
+
 #Bacteria IAA and P concentrations
 xl_1 <- "https://dfzljdn9uc3pi.cloudfront.net/2020/9005/1/DatasetS1_Raw_Data__Screening__P_solubilization_and_IAA_production.xlsx"
 download.file(xl_1, "Data/Data.xlsx", mode = "wb")
