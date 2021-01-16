26b4d71a1a38edcdb793e250492fcec75c12f6d3
Nacho Romero <nacho.romerolozano@gmail.com>
2021-01-16 23:54:05 +0100

    Updated tables, final version

--- Tables.R
+++ Tables.R
@@ -44,52 +44,54 @@ kbl(anova.dataframe, align = "c") %>%
 
 #Bean
 
-strain.bean <-  c("Control","LSM_183/LSM_24", "None","None","None", "LSM_183", "Control", "LSM_183/LSM_68/LSM_179","LSM_14", "None", "LSM_14", "None", "None")
-posthoc.bean <- c("Kruskal-Wallis", "Kruskal-Wallis","Tukey", "Duncan","Tukey", "Duncan", "Kruskal-Wallis","Kruskal-Wallis","Kruskal-Wallis","Tukey", "Duncan","Tukey", "Duncan")
-var.name.bean <- c("Plant Height", "Shoot dry mass", "Root dry mass","Root dry mass", "Total dry mass","Total dry mass", "P soil", "P shoot", "P root", "N shoot", "N shoot", "N root", "N root")
-bean.name <- c(rep("Bean", 13))
-
-data.strains.beans <- data.frame(bean.name, var.name.bean, posthoc.bean, strain.bean)
+strain.bean <-  c("Control","NA", "NA", "NA", "Control", "LSM_183/LSM_68/LSM_179","LSM_14", "NA",  "NA")
+posthoc.bean <- c("Kruskal-Wallis", "Not significant", "Not significant", "Not significant", "Kruskal-Wallis","Kruskal-Wallis","Kruskal-Wallis", "Not significant", "Not significant")
+var.name.bean <- c("Plant Height", "Shoot dry mass", "Root dry mass", "Total dry mass", "P soil", "P shoot", "P root", "N shoot", "N root")
+bean.name <- c(rep("Bean", 9))
+bean.groups <- c("a, ab, abc, abc, abc, bcd, cd, d","a, a, a, a, a, a, a, a", "a, a, a, a, a, a, a, a","a, a, a, a, a, a, a, a","a, ab, ab, b, b, b, b, b", "a, a, a, b, b, b, b, b", "a, ab, ab, ab, ab, ab, ab, b","a, a, a, a, a, a, a, a", "a, a, a, a, a, a, a, a")
+data.strains.beans <- data.frame(bean.name, var.name.bean, posthoc.bean,bean.groups, strain.bean)
 
 data.strains.beans <-  data.strains.beans %>% 
   rename("Crop" = bean.name,
          "Variable" = var.name.bean,
          "Post-hoc analysis" = posthoc.bean,
+         "Groups"= bean.groups,
          "Optimal strain" = strain.bean)
 kbl(data.strains.beans) %>% 
   kable_styling(bootstrap_options = c("hover"))%>%
   column_spec(1, bold = T) %>%
   column_spec(c(1:4), border_left = FALSE, border_right = FALSE) %>% 
-  collapse_rows(columns = 1:2, valign = "middle") %>% 
-  footnote(general = "Optimal strains for each variable measured in bean greenhouse experiments adressed by Kruskal-Wallis, Tukey or Duncan analysis.", general_title = "")
+  collapse_rows(columns = 1, valign = "middle") %>% 
+  footnote(general = "Optimal strains for each variable measured in bean greenhouse experiments adressed by Kruskal-Wallis, or Duncan analysis when significant differences are found. NA: Not applicable", general_title = "")
 
 #Maize
-strain.maize <-  c("LSM_65","LSM_65","LSM_65","LSM_65","Control", "Control", "Control/LSM_65","Control/LSM_65", "LSM_24", "LSM_24", "Control/LSM_65/LSM_14/LSM_24/LSM_62", "None", "None","LSM_65/LSM_24","LSM_65/LSM_24")
-posthoc.maize <-  c("Tukey", "Duncan","Tukey", "Duncan","Tukey", "Duncan","Tukey", "Duncan","Kruskal-Wallis","Kruskal-Wallis","Kruskal-Wallis","Tukey", "Duncan","Tukey", "Duncan" )
-var.name.maize <- c("Plant Height", "Plant Height", "Shoot dry mass","Root dry mass", "Root dry mass", "Shoot dry mass", "Total dry mass", "Total dry mass", "P soil", "P shoot", "P root","N shoot", "N shoot", "N root", "N root" )
-maize.name <-  c(rep("Maize", 15))
-
-data.strains.maize <-  data.frame(maize.name, var.name.maize, posthoc.maize, strain.maize)
+strain.maize <-  c("LSM_65","LSM_65","Control","Control/LSM_65", "LSM_24", "LSM_24", "Control/LSM_65/LSM_14/LSM_24/LSM_62", "NA","LSM_65/LSM_24")
+posthoc.maize <-  c("Duncan","Duncan","Duncan","Duncan","Kruskal-Wallis","Kruskal-Wallis","Kruskal-Wallis", "Not significant", "Duncan" )
+var.name.maize <- c("Plant Height", "Shoot dry mass","Root dry mass", "Total dry mass", "P soil", "P shoot", "P root","N shoot", "N root" )
+maize.name <-  c(rep("Maize", 9))
+maize.groups <- c("a, ab, bc, bc, cd, cd, de, e","a, ab, ab, ab, abc, bc, bc, c","a, ab, abc, abc, bc, bc, c, c","a, a, ab, ab, abc, bc, bc, c","a, ab, abc, abc, abc, bc, bc, bc, c", "a, ab, ab, bc, bc, c, c, c", "a, a, a, a, a, b, b, b", "a, a, a, a, a, a, a, a", "a,a,ab,ab,ab,ab,bc,c")
+data.strains.maize <-  data.frame(maize.name, var.name.maize, posthoc.maize,maize.groups, strain.maize)
 
 data.strains.maize <- data.strains.maize %>% 
   rename("Crop" = maize.name,
           "Variable" = var.name.maize,
           "Post-hoc analysis" = posthoc.maize,
+         "Groups"= maize.groups,
           "Optimal strain" = strain.maize)
 kbl(data.strains.maize) %>% 
   kable_styling(bootstrap_options = c("hover"))%>%
   column_spec(1, bold = T) %>%
   column_spec(c(1:4), border_left = FALSE, border_right = FALSE) %>% 
   collapse_rows(columns = 1:2, valign = "middle") %>% 
-  footnote(general = "Optimal strains for each variable measured in maize greenhouse experiments adressed by Kruskal-Wallis, Tukey or Duncan analysis.", general_title = "")
+  footnote(general = "Optimal strains for each variable measured in maize greenhouse experiments adressed by Kruskal-Wallis or Duncan analysis when significant differences are found. NA: Not applicable.", general_title = "")
 
 #Soy
-strain.soy <- c("LSM_179","LSM_179", "None","None","None", "LSM_24", "None", "LSM_24", "None", "LSM_14/LSM_24/LSM_62", "LSM_183/LSM_179","None", "None","None","None")
-posthoc.soy <- c("Tukey", "Duncan","Tukey", "Duncan","Tukey", "Duncan","Tukey", "Duncan","Kruskal-Wallis","Kruskal-Wallis","Kruskal-Wallis","Tukey", "Duncan","Tukey", "Duncan" )
-var.name.soy <- c("Plant Height", "Plant Height", "Shoot dry mass","Root dry mass", "Root dry mass", "Shoot dry mass", "Total dry mass", "Total dry mass", "P soil", "P shoot", "P root","N shoot", "N shoot", "N root", "N root" )
-soy.name <-  c(rep("Soybean", 15))
-
-data.strains.soy <-  data.frame(soy.name, var.name.soy, posthoc.soy, strain.soy)
+strain.soy <- c("LSM_179", "NA", "NA",  "NA", "All", "LSM_14/LSM_24/LSM_62", "LSM_183/LSM_179", "NA","NA")
+posthoc.soy <- c("Duncan", "Duncan", "Duncan", "Duncan","Kruskal-Wallis","Kruskal-Wallis","Kruskal-Wallis", "Duncan", "Duncan" )
+var.name.soy <- c("Plant Height",  "Shoot dry mass","Root dry mass",  "Total dry mass",  "P soil", "P shoot", "P root","N shoot",  "N root" )
+soy.name <-  c(rep("Soybean", 9))
+soy.groups <- c("a, ab, ab, abc, abc, bc, bc, c", "a, a, a, a, a, a, a, a", "a, a, a, a, a, a, a, a", "a, a, a, a, a, a, a, a", "a, a, a, a, a, a, a, a", "a, a, a, ab, ab, ab, ab, b", "a, a, ab, ab, ab, ab, ab, b", "a, a, a, a, a, a, a, a", "a, a, a, a, a, a, a, a")
+data.strains.soy <-  data.frame(soy.name, var.name.soy, posthoc.soy, soy.groups, strain.soy)
 
 data.strains.soy <- data.strains.soy %>% 
   rename("Crop" = soy.name,
@@ -101,5 +103,5 @@ kbl(data.strains.soy) %>%
   column_spec(1, bold = T) %>%
   column_spec(c(1:4), border_left = FALSE, border_right = FALSE) %>% 
   collapse_rows(columns = 1:2, valign = "middle") %>% 
-  footnote(general = "Optimal strains for each variable measured in soybean greenhouse experiments adressed by Kruskal-Wallis, Tukey or Duncan analysis.", general_title = "")
+  footnote(general = "Optimal strains for each variable measured in soybean greenhouse experiments adressed by Kruskal-Wallis, or Duncan analysis when significant differences are found. NA: Not applicable.", general_title = "")
 
